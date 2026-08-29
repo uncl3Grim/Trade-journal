b00ecd8d5a28a7f0d4aa533189c59b3a0fa4f042
uncl3Grim <fxrelentless@gmail.com>
2026-08-29 10:00:32 +0100

    Update AppShell.js

--- components/AppShell.js
+++ components/AppShell.js
@@ -2,12 +2,16 @@
 
 import Sidebar from './Sidebar';
 import MobileNav from './MobileNav';
+import BackgroundManager from './BackgroundManager';
 
 export default function AppShell({ children }) {
   return (
-    <div className="flex bg-[#f7f7fb] min-h-screen">
-      <Sidebar />
-      <div className="flex-1 pb-20 md:pb-6">{children}</div>
+    <div className="relative flex bg-[#f7f7fb] dark:bg-[#0b0b0f] min-h-screen">
+      <BackgroundManager />
+      <div className="relative z-10 flex flex-1">
+        <Sidebar />
+        <div className="flex-1 pb-20 md:pb-6">{children}</div>
+      </div>
       <MobileNav />
     </div>
   );
