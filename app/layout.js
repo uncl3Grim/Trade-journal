825e0973980d5d34bde5e87b5f41cb15e90c2f68
uncl3Grim <fxrelentless@gmail.com>
2026-08-29 02:55:20 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import './globals.css';
+import { Space_Grotesk } from 'next/font/google';
+
+const spaceGrotesk = Space_Grotesk({ subsets: ['latin'], variable: '--font-heading' });
 
 export const metadata = {
   title: 'Trade Journal',
@@ -9,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+    <body className={spaceGrotesk.variable}>
     </html>
   );
 }
